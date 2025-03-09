bc0ce69335a0d249210902a781cbb810feebbe67
SoukainaGARID <soukainagarid2004@gmail.com>
2025-03-09 13:52:58 +0000

    iscriptionFormation

--- src/beans/InscriptionFormation.java
+++ src/beans/InscriptionFormation.java
@@ -10,5 +10,45 @@ package beans;
  * @author GIGA STORE
  */
 public class InscriptionFormation {
+    private int id;
+    private Formation formation;
+    private Participant participant;
+
+    public InscriptionFormation(int id, Formation formation, Participant participant) {
+        this.id = id;
+        this.formation = formation;
+        this.participant = participant;
+    }
+
+    public InscriptionFormation(Formation formation, Participant participant) {
+        this.formation = formation;
+        this.participant = participant;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Formation getFormation() {
+        return formation;
+    }
+
+    public Participant getParticipant() {
+        return participant;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFormation(Formation formation) {
+        this.formation = formation;
+    }
+
+    public void setParticipant(Participant participant) {
+        this.participant = participant;
+    }
+    
+    
     
 }
