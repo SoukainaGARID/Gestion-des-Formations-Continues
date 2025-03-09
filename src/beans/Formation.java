92c307f4120b5b4a26c0681ad99f99b12324fbae
SoukainaGARID <soukainagarid2004@gmail.com>
2025-03-09 13:43:14 +0000

    Premier commit

---
+++ src/beans/Formation.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author GIGA STORE
+ */
+public class Formation {
+    private int id;
+    private String intitule;
+    private int duree;
+    private double cout;
+
+    public Formation(int id, String intitule, int duree, double cout) {
+        this.id = id;
+        this.intitule = intitule;
+        this.duree = duree;
+        this.cout = cout;
+    }
+
+    public Formation(String intitule, int duree, double cout) {
+        this.intitule = intitule;
+        this.duree = duree;
+        this.cout = cout;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public double getCout() {
+        return cout;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public void setCout(double cout) {
+        this.cout = cout;
+    }
+    
+    
+}
---
+++ src/beans/Participant.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author GIGA STORE
+ */
+public class Participant {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email; 
+
+    public Participant(int id, String nom, String prenom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    public Participant(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
